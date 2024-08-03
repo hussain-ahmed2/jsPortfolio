bc21b315906bec959426fdf20ec52385f9172c1a
Hussain022 <hussainahmed.vu@gmail.com>
2024-08-03 14:03:51 +0600

    nothing

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ import header from "./header.js";
 import footer from "./footer.js";
 import contents from "./contents.js";
 
+
 const root = document.getElementById('root');
+
 root.appendChild(header());
 
 root.appendChild(contents());
