9825bf90c7361cbea3ccbe7437412b7cb8efedb2
Hussain Ahmed <hussainahmed.vu@gmail.com>
2024-08-15 19:17:44 +0600

    contact page

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,31 @@
+const input = (type) => {
+  const input = document.createElement('input');
+  input.type = type;
+  return input;
+}
+
 const contact = () => {
   const contact = document.createElement('div');
-  contact.innerText = 'contact';
+  contact.id = 'contact';
+
+  const form = document.createElement('form');
+  const nameInput = input('text');
+  nameInput.placeholder = 'your name';
+  const emailInput = input('email');
+  emailInput.placeholder = 'your email';
+  const message = document.createElement('textarea');
+  message.rows = 6;
+  message.cols = 20;
+  message.placeholder = 'type here something that you have to say...';
+  const submitBtn = input('submit');
+  submitBtn.value = 'Send';
+
+  form.appendChild(nameInput);
+  form.appendChild(emailInput);
+  form.appendChild(message);
+  form.appendChild(submitBtn);
+  contact.appendChild(form);
+
   return contact;
 }
 
